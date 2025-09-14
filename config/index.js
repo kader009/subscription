478e23ce347459dfb9fdd28256e966b221e55da2
Md Abdul Kader Molla <kadermollarasel000@gmail.com>
2025-09-14 22:50:35 +0600

    env variable update

--- config/index.js
+++ config/index.js
@@ -1,5 +1,8 @@
-import { config } from 'dotenv';
-const options = { path: `.env.${process.env.NODE_ENV || 'development'}.local` };
-config(options);
+import dotenv from 'dotenv';
+const envFile = `.env.${process.env.NODE_ENV || 'development'}.local`;
+console.log('Loading env file:', envFile); // check which file it is loading
+dotenv.config({ path: envFile });
 
 export const { PORT, MONGODB_URL, NODE_ENV, JWT_SECRET, JWT_EXPIRES_IN } = process.env;
+
+console.log('MONGODB_URL:', MONGODB_URL); // check if it's loaded
